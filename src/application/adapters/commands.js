b2757afb509827ec46bd6ade72814b9c7edd08ff
Vitor Cassiano <vitor.cassiano@olxbr.com>
2020-10-23 17:56:25 -0300

    adding the nodemon for improve development environment

--- src/application/adapters/commands.js
+++ src/application/adapters/commands.js
@@ -1,30 +1,28 @@
 const { buildBossObject, buildRollObject } = require("../services")
 
-const bossHandler = bossObject => {}
-const rollHandler = () => {}
+const bossHandler = bossObject => { }
+const rollHandler = () => { }
 
 const parseContent = content => {
     const contentList = content.split(" ")
 
     try {
-        switch(contentList[0]) {
+        switch (contentList[0]) {
             case "+boss":
                 let contentObj = buildBossObject(contentList)
-                return toEveryone(`Pessoal, nasceu ${contentObj.boss} no ${contentObj.server}, vamos atacar mais tarde, fiquem ligados!`)
-                break;
+                return toEveryone(`Pessoal, nasceu **${contentObj.boss}** no **${contentObj.server}**, vamos atacar mais tarde, fiquem ligados!`)
             case "+roll":
                 let roll = buildRollObject(contentList)
                 return `O vencedor foi o número **${roll.winner}**, parabéns, você ganhou: **${roll.item}**!`
-                break;
             default:
-                return
+                return `Foi mal lek, os comandos disponíveis até o momento são: **+boss** e **+roll**`
         }
-    } catch(e) {
+    } catch (e) {
         const command = contentList[0]
         console.info({ e, command })
-        if(command === "+boss"){
+        if (command === "+boss") {
             return `Comando inválido, tente: +boss **boss-name** **server-name**`
-        } else if(command === "+roll") {
+        } else if (command === "+roll") {
             return `Comando inválido, tente: +roll **tempo-segundos** **quantidade-players** **item-sorteio**`
         } else {
             return "Comando desconhecido!"
