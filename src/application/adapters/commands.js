9da81ea4ae2a84cabf0f979c5551233c893e79bf
Vitor Cassiano <vitor.cassiano@olxbr.com>
2020-10-26 12:16:54 -0300

    refactoring the command

--- src/application/adapters/commands.js
+++ src/application/adapters/commands.js
@@ -2,18 +2,21 @@ const { alertBoss, giveaway } = require("../usecases")
 
 const commandsHandler = content => {
     try {
-        switch (contentList[0]) {
-            case content.startsWith("+boss"):
+        const command = content.split(" ")[0]
+        switch (command) {
+            case "+boss":
                 let bossMessageReply = alertBoss(content)
                 return toEveryone(bossMessageReply)
-            case content.startsWith("+roll"):
-                let rollMessageReply = giveaway(content)
-                return rollMessageReply
+            case "+roll":
+                let rollmessagereply = giveaway(content)
+                return rollmessagereply
+            case "+oi":
+                return "Hello World!"
             default:
                 return `Foi mal lek, os comandos disponíveis até o momento são: **+boss** e **+roll**`
         }
     } catch (e) {
-        const command = contentList[0]
+        const command = content.split(" ")[0]
         console.info({ e, command })
         if (command === "+boss") {
             return `Comando inválido, tente: +boss **boss-name** **server-name**`
